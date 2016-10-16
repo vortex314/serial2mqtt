36007e5060ecff4f50d4326cbd3fd23c80c6453f
vortex314 <lieven.merckx@gmail.com>
2016-10-16 23:11:48 +0200

    virtual flush Usb not called in BufferedByteSTream ?

--- inc/Usb.h
+++ inc/Usb.h
@@ -5,42 +5,34 @@
 #include "Actor.h"
 #include "BufferedByteStream.h"
 
-
-class Usb :public Actor,public BufferedByteStream
-{
+class Usb: public Actor, public BufferedByteStream {
 
 public:
-    const static int CONNECTED,DISCONNECTED,RXD,ERROR,MESSAGE,FREE;
+	const static int CONNECTED, DISCONNECTED, RXD, ERROR, MESSAGE, FREE;
 
-    Usb(const char* device) ;
-    Erc connect() ;
-    Erc disconnect() ;
-    Erc send(Bytes& bytes) ;
-    Bytes* recv() ;
+	Usb(const char* device);
+	Erc connect();
+	Erc disconnect();
+	Erc send(Bytes& bytes);
+	Bytes* recv();
 
+	void setDevice(const char* device);
+	void setBaudrate(uint32_t baud);
+	void logStats();
+	void onEvent(Cbor& cbor);
+	int fd();
+	Erc open();
+	Erc close();
+	bool hasData();
+	uint8_t read();
+	void flush();
+private:
 
-    void setDevice(const char* device) ;
-    void setBaudrate(uint32_t baud);
-    void logStats();
-    void onEvent(Cbor& cbor);
-    int fd();
-    Erc open();
-    Erc close();
-        bool hasData();
-            uint8_t read() ;
-private :
+	int _fd;
+	const char* _device;
+	uint32_t _baudrate;
+	bool _isComplete;
 
-
-    int _fd;
-    const char* _device;
-    uint32_t _baudrate;
-    bool _isComplete;
-
-    void flush();
 };
-
-
-
-
 
 #endif // USB_H
