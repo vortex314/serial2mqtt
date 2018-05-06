70ee55dde5fd74fdf3c42ab0fa76c9f35bc5b8f3
lieven <lieven.merckx@gmail.com>
2018-05-06 11:17:23 +0200

    added make paho library static

---
+++ makePaho.sh
@@ -0,0 +1,14 @@
+cc -c -g -fPIC -Os -Wall -Ibuild src/Clients.c src/Heap.c  src/MQTTAsync.c \
+src/LinkedList.c src/Log.c src/Messages.c src/MQTTClient.c \
+src/MQTTPacket.c src/MQTTPacketOut.c src/MQTTPersistence.c \
+src/MQTTPersistenceDefault.c src/MQTTProtocolClient.c \
+src/MQTTProtocolOut.c src/SocketBuffer.c src/Socket.c src/StackTrace.c \
+src/Thread.c src/Tree.c src/utf-8.c
+ar -cru build/output/libpaho-mqtt3c.a Clients.o  Heap.o MQTTAsync.o \
+LinkedList.o  Log.o  Messages.o  MQTTClient.o  MQTTPacket.o \
+MQTTPacketOut.o  MQTTPersistence.o  MQTTPersistenceDefault.o \
+MQTTProtocolClient.o  MQTTProtocolOut.o  SocketBuffer.o  Socket.o \
+StackTrace.o  Thread.o  Tree.o  utf-8.o
+cc -static -o build/output/samples/paho_cs_sub \
+src/samples/paho_cs_sub.c build/output/libpaho-mqtt3c.a -I src \
+-lpthread -L build/output
