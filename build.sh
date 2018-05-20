4a22c34a5d801c2a177ea61d627f7aec54838be7
vortex314 <lieven.merckx@gmail.com>
2018-05-20 15:38:55 +0200

    automating new deployment

---
+++ build.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/eclipse/paho.mqtt.c.git
+git clone https://github.com/pantor/inja.git
+git clone https://github.com/nlohmann/json.git
+git clone https://github.com/vortex314/Common
+git clone https://github.com/vortex314/serial2mqtt
+make -f serial2mqtt.mk
+cd Common
+make -f Common.mk
+cd ..
+cd paho.mqtt.c
+cp ../serial2mqtt/makePaho.sh .
+make
+./makePaho.sh 
+cd ../serial2mqtt
+make -f serial2mqtt.mk
