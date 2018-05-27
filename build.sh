51daf8e7dd182e025c45ee2ae9b060549096d7c6
vortex314 <lieven.merckx@gmail.com>
2018-05-27 10:12:31 +0200

    working on auto build

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ git clone https://github.com/pantor/inja.git
 git clone https://github.com/nlohmann/json.git
 git clone https://github.com/vortex314/Common
 git clone https://github.com/vortex314/serial2mqtt
-make -f serial2mqtt.mk
-cd Common
+cd ../Common
 make -f Common.mk
 cd ../paho.mqtt.c
 make
