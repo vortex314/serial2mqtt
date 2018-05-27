8f9bd467ec303b24309c774b51b767e766f26af2
vortex314 <lieven.merckx@gmail.com>
2018-05-27 10:13:57 +0200

    building

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-sudo apt-get install libssl-dev
+#  INSTALL OPENSSL dev libraries first
+# sudo apt-get install libssl-dev
+#
 git clone https://github.com/eclipse/paho.mqtt.c.git
 git clone https://github.com/pantor/inja.git
 git clone https://github.com/nlohmann/json.git
 git clone https://github.com/vortex314/Common
 git clone https://github.com/vortex314/serial2mqtt
-cd ../Common
+cd Common
 make -f Common.mk
 cd ../paho.mqtt.c
 make
