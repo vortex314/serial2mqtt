7ec077bb9ff253b8b8217daf72dbdee989555027
vortex314 <lieven.merckx@gmail.com>
2018-10-27 14:39:28 +0200

    update paho static make

--- makePaho.sh
+++ makePaho.sh
@@ -3,12 +3,10 @@ src/LinkedList.c src/Log.c src/Messages.c src/MQTTClient.c \
 src/MQTTPacket.c src/MQTTPacketOut.c src/MQTTPersistence.c \
 src/MQTTPersistenceDefault.c src/MQTTProtocolClient.c \
 src/MQTTProtocolOut.c src/SocketBuffer.c src/Socket.c src/StackTrace.c \
-src/Thread.c src/Tree.c src/utf-8.c
+src/Thread.c src/Tree.c src/utf-8.c src/MQTTProperties.c src/WebSocket.c src/SHA1.c src/Base64.c 
 ar -cru build/output/libpaho-mqtt3c.a Clients.o  Heap.o MQTTAsync.o \
 LinkedList.o  Log.o  Messages.o  MQTTClient.o  MQTTPacket.o \
 MQTTPacketOut.o  MQTTPersistence.o  MQTTPersistenceDefault.o \
 MQTTProtocolClient.o  MQTTProtocolOut.o  SocketBuffer.o  Socket.o \
-StackTrace.o  Thread.o  Tree.o  utf-8.o
-cc -static -o build/output/samples/paho_cs_sub \
-src/samples/paho_cs_sub.c build/output/libpaho-mqtt3c.a -I src \
--lpthread -L build/output
+StackTrace.o  Thread.o  Tree.o MQTTProperties.o WebSocket.o SHA1.o Base64.o utf-8.o
+# cc -static -o build/output/samples/paho_cs_sub src/samples/paho_cs_sub.c build/output/libpaho-mqtt3c.a -I src -lpthread -L build/output
