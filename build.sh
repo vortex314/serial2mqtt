999cfe24ed5a8c0e9aa09efc825a5f43e44f7c2c
vortex314 <lieven.merckx@gmail.com>
2020-12-13 19:54:37 +0100

    updated build instructions

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 echo "run from workspace directory !"
 #  INSTALL OPENSSL dev libraries first
-# sudo apt-get install libssl-dev
+sudo apt-get install libssl-dev
 #
 git clone https://github.com/eclipse/paho.mqtt.c.git
-git clone https://github.com/pantor/inja.git
-git clone https://github.com/nlohmann/json.git
+git clone https://github.com/bblanchon/ArduinoJson
 git clone https://github.com/vortex314/Common
 git clone https://github.com/vortex314/serial2mqtt
 cd Common
@@ -18,3 +17,7 @@ cd ../serial2mqtt
 # change all paths in the .mk file to your paths
 #
 make -f serial2mqtt.mk
+# for the build of the binary copy
+sudo apt-get install zip
+make -f serial2mqtt.mk PostBuild
+ 
